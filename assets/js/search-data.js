5558fa3bf0316259daf7bbedd0a770917074b064
yifanjiang00 <jiangyifan1807@163.com>
2026-01-24 06:27:02 +0000

    Deploying to gh-pages from @ yifanjiang00/yifanjiang00.github.io@ac82d72a61673a95a483fb537fce91c4cdf8b36e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -429,16 +429,11 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+            },},{id: "news-i-won-the-first-prize-in-the-college-students-higher-mathematics-competition-of-zhejiang-province",
+          title: 'I won the first prize in the College Students’ Higher Mathematics Competition of...',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          section: "News",},{id: "news-i-received-the-national-scholarship",
+          title: 'I received the national scholarship!',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
@@ -507,35 +502,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("mailto:%79%69%66%61%6E%6A%36%37%30@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=WTqTBU8AAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
